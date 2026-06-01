4b0bf5009e227fdd28d72f4a4485f6a6c7f0cb14
jackwang2088-max <jackwang2088@gmail.com>
2026-06-01 20:54:26 +0800

    Update app_Render.py

--- app_Render.py
+++ app_Render.py
@@ -183,7 +183,7 @@ def webhook():
         send_to_telegram(telegram_message)
         print("Webhook耗時 =", time.time() - start_time)
         #send_to_local_speaker({"id": eid, "data": data})
-        eturn jsonify({"status": "success", "id": eid}), 200
+        return jsonify({"status": "success", "id": eid}), 200
 
         # =====================
         # 本地 Speaker
