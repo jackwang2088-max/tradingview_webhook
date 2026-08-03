76ef9804ad655bc4eb6bf19c48350bfb4f2240b1
jackwang2088-max <jackwang2088@gmail.com>
2026-08-04 07:23:46 +0800

    Update app_Render.py

--- app_Render.py
+++ app_Render.py
@@ -242,7 +242,7 @@ print("worker_thread =", worker_thread)
 print("alive =", worker_thread.is_alive())
 print("ident =", worker_thread.ident)
 print("native =", worker_thread.native_id)
-print("========================"
+print("========================")
 
 
 # =============================================================================
